b059713adbf20fa8de92786bab1c27eb54dd2028
Clovis <dabaclovis@yahoo.com>
2022-02-09 18:14:27 -0500

    first

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="list-group">
+        <div class="list-group-item list-group-item-action flex-column align-items-start card mb-2">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Covid crises in America</h5>
+                <small>Clovis</small>
+            </div>
+            <blockquote class="blockquote">
+                <p class="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Dolore, voluptatum vel. Enim natus cupiditate vitae amet? Accusamus inventore
+                      quae aperiam, sunt obcaecati reprehenderit nam cumque sed ab omnis sapiente
+                     velit quas consequuntur, officia minus provident.</p>
+            </blockquote>
+            <small>paragraph footer</small>
+        </div>
+        <div class="list-group-item list-group-item-action flex-column align-items-start card mb-2">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Poverty in Africa</h5>
+                <small>Clovis</small>
+            </div>
+            <blockquote class="blockquote">
+                <p class="mb-1">Lorem ipsum dolor sit amet consectetur
+                     adipisicing elit. Dolore, voluptatum vel. Enim natus cupiditate
+                      vitae amet? Accusamus inventore quae aperiam, sunt obcaecati
+                       reprehenderit nam cumque sed ab omnis sapiente velit quas consequuntur, officia minus provident.</p>
+            </blockquote>
+            <small>paragraph footer</small>
+        </div>
+        <div class="list-group-item list-group-item-action flex-column align-items-start card mb-2">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">The leading cause of dead in Africa</h5>
+                <small>Clovis</small>
+            </div>
+            <blockquote class="blockquote">
+                <p class="mb-1">Lorem ipsum dolor sit amet consectetur
+                     adipisicing elit. Dolore, voluptatum vel. Enim natus cupiditate vitae
+                      amet? Accusamus inventore quae aperiam, sunt obcaecati reprehenderit
+                       nam cumque sed ab omnis sapiente velit quas consequuntur, officia minus provident.</p>
+            </blockquote>
+            <small>paragraph footer</small>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-3">Jumbo heading</h1>
+        <blockquote class="blockquote">
+            <p class="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Ducimus nisi dolorum nulla deserunt, vel magnam, fugiat recusandae optio odit officia
+                assumenda ut amet illum harum nobis tempora? Velit magni
+                minus quam accusamus dolor omnis possimus obcaecati, officia nemo, quisquam ducimus.</p>
+        </blockquote>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="/articles" role="button">More here</a>
+        </p>
+    </div>
+@endsection
